771c4435d8a1e0addfd5d4d2261ad1bd1a1df1f6
ViChungg <43527217+ViChungg@users.noreply.github.com>
2018-12-17 20:07:05 -0800

    Add files via upload

---
+++ js/abb.js
@@ -0,0 +1,11 @@
+/*js for abb*/
+
+// Simple list
+Sortable.create(simpleList, { /* options */ });
+Sortable.create(simpleList2, { /* options */ });
+
+// List with handle
+Sortable.create(listWithHandle, {
+  handle: '.glyphicon-move',
+  animation: 150
+});

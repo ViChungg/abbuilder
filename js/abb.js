ec46f5220bae2edd389ff9ab454fff1fca764204
Vicky Chung <vchung22@my.bcit.ca>
2018-12-17 20:09:33 -0800

    Deleted routine builder

--- js/abb.js
+++ js/abb.js
@@ -1,11 +1,1 @@
 /*js for abb*/
-
-// Simple list
-Sortable.create(simpleList, { /* options */ });
-Sortable.create(simpleList2, { /* options */ });
-
-// List with handle
-Sortable.create(listWithHandle, {
-  handle: '.glyphicon-move',
-  animation: 150
-});
